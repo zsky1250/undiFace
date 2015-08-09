9de6547bf76c5657f881575ad30b5a4edc055275
zwr <zhangwr0226@gmail.com>
2015-08-10 00:41:45 +0800

    Undi工程的UI 打算做3套，以后备用

---
+++ ReadMe.md
@@ -0,0 +1,7 @@
+Undifition的UI项目
+专注于我们的弱项--UI建立。目前主要是 收集整理创作自己的页面HTML、CSS和JS代码。附加简单的后台数据，希望做几套能够拿来就用的前台样式
+Todo：
+目前三个方案三个版本，有空就完善:
+zUIA：BootStrap+fontawesome的最潮后台，为系浏览器设计。橙色为主。春DIV+JS脚本控制调准。AJAX取数据，前台组件化...
+zUIB：AmazeUI贴近用户习惯的UI 蓝色为主。为适应体，希望提供一个大众色，大众方案。但是不兼容IE6。DIV+AJAX
+zUIC：FrameSet写的，主要目的是为了兼容IE6，一些政府部门就要用IE6，我们只能呵呵了....
---
+++ src/main/java/com/udf/controller/pageController.java
@@ -0,0 +1,31 @@
+package com.udf.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Created by ’≈Œ¥»ª on 2015/8/8.
+ */
+@Controller
+public class pageController {
+
+    @RequestMapping("/{url}")
+    public String index(@PathVariable String url){
+        return "index";
+    }
+
+
+    @RequestMapping("/zUIC/*")
+    public void zUIC(HttpServletRequest request,Model model){
+        String basePath = request.getContextPath();
+        System.out.println("sdf:++++++"+basePath+"/res/zUIC");
+        model.addAttribute("res",basePath+"/res/zUIC");
+//      return "/zUIC/"+url;
+    }
+
+
+}
